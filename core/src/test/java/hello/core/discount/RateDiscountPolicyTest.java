e41321c2f6422e749fefed2707aff8280badaf4c
cwkim723 <jerry.pref@gmail.com>
2022-02-01 00:55:17 +0900

    feat/core: 새로운 할인 정책 개발

---
+++ core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,51 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다.")
+    void vip_o(){
+        // given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+
+        // when
+        int discount = discountPolicy.discount(member, 10000);
+
+        // then
+        assertThat(discount).isEqualTo(1000);
+    }
+
+    // 실패 테스트
+    @Test
+    @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다.")
+    void vip_x(){
+        // given
+        Member member = new Member(2L, "memberBASIC", Grade.BASIC);
+
+        // when
+        int discount = discountPolicy.discount(member, 10000);
+
+        // then
+        assertThat(discount).isEqualTo(1000); // alt enter -> add on-demand static import for ~ ==> import static org.assertj.core.api.Assertions.*; 이렇게 임포트 하는 것이 좋다
+//        Assertions.assertThat(discount).isEqualTo(1000);
+
+        /*
+          expected: 1000
+          but was: 0
+          Expected :1000
+          Actual   :0
+         */
+    }
+
+}
