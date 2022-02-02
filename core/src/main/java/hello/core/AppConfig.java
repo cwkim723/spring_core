4713731e1530ab1e7f6c32ae814b1ccb8b037353
cwkim723 <jerry.pref@gmail.com>
2022-02-03 00:15:30 +0900

    feat/core: 스프링으로 전환하기

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -2,14 +2,20 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    // @Configuration: 설정 정보
+    // @Bean: 각 메소드가 spring container에 등록됨
 
 //    public MemberService memberService(){
 //        return new MemberServiceImpl(new MemoryMemberRepository());
@@ -19,20 +25,25 @@ public class AppConfig {
 //        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
 //    }
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository()); // refactor: ctrl alt m
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,27 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        // 방법 1
 //        MemberService memberService = new MemberServiceImpl();
+
+        // 방법 2
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // 방법 3
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // AppConfig에서 @Configuration 설정해준 것을 AnnotationConfigApplicationContext이 JavaConfig 클래스를 읽어들여 IoC와 DI를 적용해줌
+        // 설정파일 중에 @Bean이 붙어 있는 메소드들을 AnnotationConfigApplicationContext이 자동으로 실행해 그 결과로 리턴하는 객체들을 기본적으로 싱글턴으로 관리하게 됨
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        //                                     이름               타입
+
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
