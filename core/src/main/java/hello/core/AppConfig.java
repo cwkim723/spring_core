126b0c91b7f1bd93b45be961fe4a57c659a88787
cwkim723 <jerry.pref@gmail.com>
2022-02-02 18:08:25 +0900

    refactor/core: AppConfig 리팩토링

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,12 +11,28 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+//    public MemberService memberService(){
+//        return new MemberServiceImpl(new MemoryMemberRepository());
+//    }
+//
+//    public OrderService orderService(){
+//        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+//    }
+
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository()); // 나는 MemberServiceImpl인데 MemoryMemberRepository를 쓸 거야를 여기서 선언해줌
+        return new MemberServiceImpl(memberRepository()); // refactor: ctrl alt m
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 
 }
