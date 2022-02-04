8da844dcaed0b6265c897bce98429eb4855fedff
cwkim723 <jerry.pref@gmail.com>
2022-02-04 17:54:00 +0900

    feat: 싱글톤 패턴

--- core/src/test/java/hello/core/singleton/SingletonTest.java
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SingletonTest {
 
     @Test
@@ -26,7 +28,25 @@ public class SingletonTest {
         // 다른 객체 값을 가져오는 것을 확인할 수 있음 -> 효율 떨어짐
 
         // memberService1 != memberService2
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
         // 통과함 -> true
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+        // singletonService1 = hello.core.singleton.SingletonService@63475ace
+        // singletonService2 = hello.core.singleton.SingletonService@63475ace
+        // 같은 인스턴스 반환
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+        // same: == (객체 인스턴스가 같은지 비교)
+        // equal: 자바 equals
+        // 테스트 통과 -> true
     }
 }
