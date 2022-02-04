8ef14ab619585d22246d1db289c92f643bd824bd
cwkim723 <jerry.pref@gmail.com>
2022-02-04 18:07:36 +0900

    feat: 싱글톤 컨테이너

--- core/src/test/java/hello/core/singleton/SingletonTest.java
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -48,5 +50,23 @@ public class SingletonTest {
         // same: == (객체 인스턴스가 같은지 비교)
         // equal: 자바 equals
         // 테스트 통과 -> true
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer(){
+//        AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println("memberService1 : " + memberService1);
+        System.out.println("memberService2 : " + memberService2);
+        // memberService1 : hello.core.member.MemberServiceImpl@61ce23ac
+        // memberService2 : hello.core.member.MemberServiceImpl@61ce23ac
+        // 참조값이 같다
+
+        assertThat(memberService1).isSameAs(memberService2);
+        // 통과함 -> true
     }
 }
