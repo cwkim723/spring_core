f042c3b9fa6e011ae64d7cea1807230c11ca99a6
cwkim723 <jerry.pref@gmail.com>
2022-02-13 22:46:17 +0900

    feat: 중복 등록과 충돌

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -18,4 +21,9 @@ public class AutoAppConfig {
     /* @ComponentScan
         @Component가 붙은 클래스를 찾아서 자동으로 스프링 빈으로 등록시켜줌
     */
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 }
