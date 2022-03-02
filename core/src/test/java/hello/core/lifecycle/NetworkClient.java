5346058d0e42e9c2eb3ff25e1ab46167630845ef
cwkim723 <jerry.pref@gmail.com>
2022-03-03 00:51:04 +0900

    feat: 빈 등록 초기화, 소멸 메서드

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -29,17 +26,13 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close : " + url);
     }
 
-    // InitializingBean 관련
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() {
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메시지");
     }
 
-    // DisposableBean 관련
-    @Override
-    public void destroy() throws Exception {
+    public void close() {
         System.out.println("NetworkClient.destroy");
         disConnect();
     }
