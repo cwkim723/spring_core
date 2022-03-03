a342df79de0f688d885ca997af4ce8ee9dc69717
cwkim723 <jerry.pref@gmail.com>
2022-03-04 01:20:01 +0900

    feat: 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 문제 해결

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,12 +1,15 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,21 +37,31 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 3. 만들어진 PrototypeBean이 할당됨 => 생성시점에 주입
+//        private final PrototypeBean prototypeBean; // 3. 만들어진 PrototypeBean이 할당됨 => 생성시점에 주입 x01
+
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean) { // 1. 이때 스프링 컨테이너에 PrototypeBean을 요청함 => 2. 스프링 컨테이너가 PrototypeBean을 만듦 => 이제 또 호출할 경우 지금 만들어진 PrototypeBean을 할당할 예정임
+//            this.prototypeBean = prototypeBean;
+//        }
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) { // 1. 이때 스프링 컨테이너에 PrototypeBean을 요청함 => 2. 스프링 컨테이너가 PrototypeBean을 만듦 => 이제 또 호출할 경우 지금 만들어진 PrototypeBean을 할당할 예정임
-            this.prototypeBean = prototypeBean;
-        }
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider; // getObject()을 호출하면 그 때 컨테이너에서 PrototypeBean을 찾아서 반환
+        private Provider<PrototypeBean> provider;
 
         public int logic(){
-            prototypeBean.addCount(); // 생성시점에 주입된 그 prototypeBean을 씀
-            return prototypeBean.getCount(); // ctrl alt n => 합쳐줌 (int count = ~ / return count)
+//            prototypeBean.addCount(); // 생성시점에 주입된 그 prototypeBean을 씀
+//            return prototypeBean.getCount(); // ctrl alt n => 합쳐줌 (int count = ~ / return count)
+
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = provider.get();
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
         }
     }
 
